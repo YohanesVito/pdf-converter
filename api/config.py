81164ceb93947ec6f03d80a4582db9ffebc983ff
YohanesVito <yohanes.vito@ti.ukdw.ac.id>
2025-06-05 06:28:03 +0700

    fix: add missing origin to ALLOWED_ORIGINS and remove unused output property in next.config.js

--- api/config.py
+++ api/config.py
@@ -6,6 +6,7 @@ API_BASE_URL = os.getenv("API_BASE_URL", "")
 
 ALLOWED_ORIGINS = [
     "https://tech.tugra-dev.my.id",
+    "https://dev.tugra-dev.my.id"
     "http://localhost:3000",
     "http://192.168.100.2:3000"
 ]
